9f0252c6b09d44fd9213d9ae0d2ea2685280437f
Maya360 <alanisespinoza13@gmail.com>
2026-03-23 18:00:55 -0600

    Prueba

---
+++ src/Dao/Books/Home.php
@@ -0,0 +1,20 @@
+<?php
+namespace Dao\Books;
+
+use Dao\Table;
+
+class Home extends Table
+{
+  /*
+    public static function getFeaturedBooks()
+    {
+     
+      
+
+        return self::obtenerRegistros($sqlstr, $params);
+    }
+        */
+
+    
+}
+?>
