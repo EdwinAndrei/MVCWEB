dd68a33c1fdc809b644c9c75d817cf5ea9a5b407
salmeron51 <salmerongladys51@gmail.com>
2026-04-15 22:04:11 -0600

    Refactor Listado class for compras listing

--- src/Controllers/Listado.php
+++ src/Controllers/Listado.php
@@ -1,53 +1,34 @@
 <?php
+// CRUD. (Create, Read, Update Delete)
+//       ( Insert, Select, Update, Delete)
+//       ( INS, DSP, UPD, DEL)
 
 namespace Controllers\Historial\Compras;
 
 use Controllers\PublicController;
 use Views\Renderer;
-use Utilities\Site;
 
 use Dao\Historial\Compras as ComprasDAO;
 
-const COMPRAS_DETALLE_VIEW_TEMPLATE = "historial/compras/detalle";
-const COMPRAS_LISTADO_URL           = "index.php?page=Historial_Compras_Listado";
+const COMPRAS_LIST_VIEW_TEMPLATE = "historial/compras/listado";
 
-class Detalle extends PublicController
+class Listado extends PublicController
 {
 
-    private array $viewData = [];
-
-    private $id;
-    private array $compra   = [];
-    private array $detalle  = [];
+    private array $comprasList = [];
 
     public function run(): void
     {
-        $this->LoadPage();
-        $this->GenerarViewData();
-        Renderer::render(COMPRAS_DETALLE_VIEW_TEMPLATE, $this->viewData);
+        $this->comprasList = ComprasDAO::getAllCompras();
+        Renderer::render(COMPRAS_LIST_VIEW_TEMPLATE, $this->prepareViewData());
     }
 
-    private function LoadPage()
+    private function prepareViewData()
     {
-        $this->id = intval($_GET["id"] ?? '0');
-        if ($this->id <= 0) {
-            Site::redirectToWithMsg(COMPRAS_LISTADO_URL, "Error al cargar el detalle, Se requiere Id de la Compra");
-        }
-        $this->compra = ComprasDAO::getCompraById($this->id);
-        if (count($this->compra) <= 0) {
-            Site::redirectToWithMsg(COMPRAS_LISTADO_URL, "No se encontró la Compra");
-        }
-        $this->detalle = ComprasDAO::getDetalleByCompraId($this->id);
-    }
-
-    private function GenerarViewData()
-    {
-        $this->viewData["id"]       = $this->compra["id"];
-        $this->viewData["usercod"]  = $this->compra["usercod"];
-        $this->viewData["username"] = $this->compra["username"];
-        $this->viewData["fecha"]    = $this->compra["fecha"];
-        $this->viewData["total"]    = $this->compra["total"];
-        $this->viewData["detalle"]  = $this->detalle;
+        return [
+            "compras"     => $this->comprasList,
+            "showDetalle" => true
+        ];
     }
 }
 
